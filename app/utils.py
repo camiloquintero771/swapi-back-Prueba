e5053c68832acc81503394ad83099c688cac1d3a
camilo <quinterocamilo771@gmail.com>
2022-03-02 15:37:58 -0500

    Docstring de funcion en utils
    Documentar con un docstring de  la función ubicada en swapi/app/utils llamada:
    "generic_model_mutation_process".

--- app/utils.py
+++ app/utils.py
@@ -1,5 +1,25 @@
 def generic_model_mutation_process(model, data, id=None, commit=True):
+    """
+        La función crea y actualiza objetos utilizando los siguientes
+        parametros
 
+        model: Model de Django:.
+        data: Dict con los fields para el objeto a creat/actualizar.
+        id: Int para buscar el objeto a actualizar
+        commit: Indica si se debe guardar el objeto.
+        model instance.
+
+        ACTUALIZAR Si el atributo de clave principal del objeto se establece en un
+         valor que se evalúa como True (es decir esxite en la base de datos).
+
+        CREAR si el atributo de clave principal del objeto no está configurado o
+         si la ACTUALIZACIÓN no actualizó nada (por ejemplo, si la clave principal
+         está configurada no existe en la base de datos).
+         Crea un nuevo objeto con las ** kwargs (data),
+         guardandolo en la base de datos y retornando el objeto creado.
+
+        No tiene restriccion alguna en el uso de la funcion.
+    """
     if id:
         item = model.objects.get(id=id)
         try:
